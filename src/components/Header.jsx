f74ec0e344ed9d77829fd667deacf327d15a04bb
AdriMan <adrimpartner@gmail.com>
2022-03-09 17:04:01 +0100

    change header title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
 
       <header className="mx-auto bg-teal-500 py-3">
         <h1 className="font-black text-white text-4xl text-center">
-          Seguimiento de <span className="text-indigo-700">Pacientes</span>
+          Generador de <span className="text-indigo-700">Pacientes</span>
           </h1>
       </header>
 
